af2f1a2d279375c57131b03b128b1a570ed674f6
Sammy Liu <xsl.sammy@gmail.com>
2024-07-25 12:59:54 -0400

    axis label & tooltip

--- MPG-scatter.js
+++ MPG-scatter.js
@@ -6,20 +6,20 @@ async function init() {
   data = await d3.csv('https://flunky.github.io/cars2017.csv');
 
   //outer (include axis) 700x800, inner (only chart data) 600x600
-  margin = { x: 100, y: 50 }; //padding
+  margin = { left: 150, right: 50, top: 25, bottom: 75 }; //padding
   inner_chart = { x: 600, y: 600 };
 
   //domain:  values of x axis (# of cylinders), range of inner chart
   x = d3.scaleLinear().domain([-1, 12]).range([0, inner_chart.x]);
   //domain: values of y axis (cityMPG), range of inner chart
   y = d3.scaleLinear().domain([0, 150]).range([inner_chart.y, 0]);
-//   y = d3.scaleLog([10, 150], [inner_chart.y, 0]).base(10);
+  //   y = d3.scaleLog([10, 150], [inner_chart.y, 0]).base(10);
 
   //scatter plot
   //create g
   d3.select('svg')
     .append('g')
-    .attr('transform', 'translate(' + margin.x + ',' + margin.y + ')') //move whole chart
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')') //move whole chart
     .selectAll('circle') // create circle
     .data(data) //import data
     .enter()
@@ -40,7 +40,8 @@ async function init() {
   //y axis
   d3.select('svg')
     .append('g')
-    .attr('transform', 'translate(' + margin.x + ',' + margin.y + ')')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+    .style('font-size', 20)
     .call(d3.axisLeft(y));
 
   //x axis, y translate value is range+margin (need to be placed at bottom)
@@ -48,7 +49,27 @@ async function init() {
     .append('g')
     .attr(
       'transform',
-      'translate(' + margin.x + ',' + (inner_chart.y + margin.y) + ')'
+      'translate(' + margin.left + ',' + (inner_chart.y + margin.top) + ')'
     )
+    .style('font-size', 14)
     .call(d3.axisBottom(x));
+
+  //y axis label
+  d3.select('svg')
+    .append('text')
+    .attr('text-anchor', 'end')
+    .attr('transform', 'rotate(-90)')
+    .attr('y', margin.left - 120)
+    .attr('x', margin.top - 275)
+    .style('font-size', 18)
+    .text(value);
+
+  //x axis label
+  d3.select('svg')
+    .append('text')
+    .attr('text-anchor', 'end')
+    .attr('x', margin.left + 350)
+    .attr('y', inner_chart.y + margin.top + 50)
+    .style('font-size', 18)
+    .text('Engine Cylinders');
 }
--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-function averageData(cylinders, data) {
-  var avgData = cylinders.map((c) => {
-    const cyData = data.filter((d) => {
-      return d.EngineCylinders == c;
-    });
-
-    // const meanEngineCylinders = d3.mean(
-    //   cyData.map((d) => {
-    //     return d.EngineCylinders;
-    //   })
-    // );
-    const makeList = cyData.map((d) => {
-      return d.Make;
-    });
-
-    return {
-      EngineCylinders: c,
-      makeList: makeList,
-      meanAverageCityMPG: meanAverageCityMPG,
-      meanAverageHighwayMPG: meanAverageHighwayMPG,
-      AverageMPG: (meanAverageCityMPG + meanAverageHighwayMPG) / 2.0,
-    };
-  });
-
-  return avgData;
-}
-
 function addFuelList(data) {
   var newData = [];
   var FuelDict = {}; //'MakeCylinderCount': [Fuels]
@@ -63,9 +36,9 @@ async function init() {
     ),
   ];
 
-  //   data = averageData(cylinders, data);
+  //added fuel list to data
   data = addFuelList(data);
-  console.log(data);
+  // console.log(data);
 
   // sort based on # cylinders
   data.sort((a, b) => {
@@ -73,7 +46,7 @@ async function init() {
   });
 
   //outer (include axis) 700x800, inner (only chart data) 600x600
-  margin = { x: 100, y: 50 }; //padding
+  margin = { left: 150, right: 50, top: 25, bottom: 75 }; //padding
   inner_chart = { x: 600, y: 600 };
 
   //domain:  values of x axis (# of cylinders), range of inner chart
@@ -102,11 +75,45 @@ async function init() {
     ['red', 'orange', 'blue']
   );
 
+  // create a tooltip
+  tooltip = d3
+    .select('#chart')
+    .append('div')
+    .style('opacity', 0)
+    .attr('class', 'tooltip')
+    .style('background-color', 'white')
+    .style('border', 'solid')
+    .style('border-width', '1px')
+    .style('border-radius', '5px')
+    .style('padding', '10px')
+    .style('position', 'absolute');
+
+  // Three function that change the tooltip when user hover / move / leave a cell
+  mouseover = function (d) {
+    console.log(d)
+    tooltip.style('opacity', 1);
+    d3.select(this).style('stroke', 'black');
+  };
+
+  mousemove = function (d) {
+    console.log(d3.mouse(this));
+    tooltip
+      .html('The exact value of ' + d.EngineCylinders)
+      .style('left', d3.mouse(this)[0] + margin.left + 20 + 'px') // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
+      .style('top', d3.mouse(this)[1] +125 + 'px');
+  };
+
+  // A function that change this tooltip when the leaves a point: just need to set opacity to 0 again
+  mouseleave = function (d) {
+    tooltip.style('opacity', 0);
+    d3.select(this).style('stroke', 'none');
+  };
+
   //scatter plot
   //create g
-  d3.select('svg')
+  d3.select('#chart').select('svg')
     .append('g')
-    .attr('transform', 'translate(' + margin.x + ',' + margin.y + ')') //move whole chart
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')') //move whole chart
     .selectAll('circle') // create circle
     .data(data) //import data
     .enter()
@@ -130,19 +137,26 @@ async function init() {
       }
       return color(d.Fuel);
     })
+    // change opacity of color based on fuel amount
     .style('opacity', function (d, i) {
-        if (d.FuelList.length >1) {
+      if (d.FuelList.length > 1) {
         return 0.7;
-        } else {
+      } else {
         return 0.5;
-        }
-    });
-  // .style('opacity', 0.7);
+      }
+    })
+    // .style('opacity', 0.7)
+    .on('mouseover', mouseover)
+    .on('mousemove', mousemove)
+    .on('mouseleave', mouseleave);
 
   //y axis
   d3.select('svg')
     .append('g')
-    .attr('transform', 'translate(' + margin.x + ',' + (margin.y - 5) + ')')
+    .attr(
+      'transform',
+      'translate(' + margin.left + ',' + (margin.top - 5) + ')'
+    )
     .call(d3.axisLeft(y));
 
   //x axis, y translate value is range+margin (need to be placed at bottom)
@@ -150,7 +164,29 @@ async function init() {
     .append('g')
     .attr(
       'transform',
-      'translate(' + margin.x + ',' + (inner_chart.y + margin.y) + ')'
+      'translate(' + margin.left + ',' + (inner_chart.y + margin.top) + ')'
     )
+    .style('font-size', 14)
     .call(d3.axisBottom(x));
+
+  //y axis label
+  d3.select('svg')
+    .append('text')
+    .attr('text-anchor', 'end')
+    .attr('transform', 'rotate(-90)')
+    .attr('y', margin.left - 120)
+    .attr('x', margin.top - 325)
+    .style('font-size', 18)
+    .text('Make');
+
+  //x axis label
+  d3.select('svg')
+    .append('text')
+    .attr('text-anchor', 'end')
+    .attr('x', margin.left + 350)
+    .attr('y', inner_chart.y + margin.top + 50)
+    .style('font-size', 18)
+    .text('Engine Cylinders');
+
+  
 }
